b0bfdbb61a4f4d07550f9633372c274f96524087
iamkaleb <kalebdmoran@gmail.com>
2020-08-05 15:29:04 -0500

    Added details pages for all three resources

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('books/form', book_form, name='book_form'),
     path('libraries/form', library_form, name='library_form'),
+    path('books/<int:book_id>/', book_details, name='book'),
+    path('libraries/<int:library_id>/', library_details, name='library'),
+    path('librarians/<int:librarian_id>/', librarian_details, name='librarian'),
 ]
