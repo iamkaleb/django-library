56cda586724567e091106e5ce95a4f67ec72d3e0
iamkaleb <kalebdmoran@gmail.com>
2020-08-05 14:32:15 -0500

    book and library forms added

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('librarians/', list_librarians, name='librarians'),
     path('libraries/', list_libraries, name='libraries'),
     path('logout/', logout_user, name='logout'),
+    path('books/form', book_form, name='book_form'),
+    path('libraries/form', library_form, name='library_form'),
 ]
